dfcdc35abcc325198ed9b56d3c53af2493b4741e
Rabea Gleissner <rabeagleissner@gmail.com>
2021-01-12 17:30:35 +0800

    WIP: rename trebuchet to catapult

--- example/client/index.js
+++ example/client/index.js
@@ -5,7 +5,7 @@ import s3rverConfiguration from '../config';
 import '../fake-s3';
 
 const app = express();
-const s3Trebuchet = initS3Trebuchet(s3rverConfiguration);
+const s3Catapult = initS3Trebuchet(s3rverConfiguration);
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'client.html')));
 app.put('/test-multipart-params', s3Trebuchet.multipartParamsHandler);
 app.put('/test-validate/:fileKey', s3Trebuchet.fileValidationHandler('fileKey'));
